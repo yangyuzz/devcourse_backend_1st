0c37b757629c310f6522b581307c2ad5589ec31f
Yangyu <ssunyujin@naver.com>
2024-08-30 16:34:38 +0900

    at 0830 pm

--- work-algo/algo_code/src/day0830_재귀_bfs/Main_15683_감시.java
+++ work-algo/algo_code/src/day0830_재귀_bfs/Main_15683_감시.java
@@ -1,6 +1,11 @@
 package day0830_재귀_bfs;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main_15683_감시 {
     static int[] di = { -1,1,0,0 }; // 0:상 1:하 2:좌 3:우
@@ -34,8 +39,77 @@ public class Main_15683_감시 {
         }
     }
     static int N, M; // 지도크기
-    static ArrayList<CCTV> camera; // 카메라 총 몇개있을지 입력 안줌.
+    static ArrayList<CCTV> cameras; // 카메라 총 몇개있을지 입력 안줌.
     static int ans; // 카메라를 적절히 배치했을 때 사각지대의 최소크기
 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        int[][] map = new int[N][M];
+        cameras = new ArrayList<>();
+        ans = Integer.MAX_VALUE;
+
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                map[i][j] = sc.nextInt();
+                if(map[i][j]>=1 && map[i][j]<=5){ // 카메라다 ! 모아 !
+                    cameras.add(new CCTV(i, j, map[i][j]));
+                }
+            }
+        }// end input
+
+        dfs(0, map); // (카메라 인덱스, 현재 상태의 맵을 주자)
+        System.out.println(ans);
+    }
     
+    static void dfs(int camIdx, int[][] prev){
+        if(camIdx == cameras.size()){ // 모든 카메라가 배치된 상태네? 사각지대 체크하자
+            int zero = 0;
+            for(int i=0; i<N; i++){
+                for(int j=0; j<M; j++){
+                    if(prev[i][j] == 0) zero++;
+                }
+            }
+            ans = Math.min(ans, zero);
+            return;
+        }
+        // 현재 내가 배치해야 하는 카메라 종류 확인
+        CCTV cctv = cameras.get(camIdx);
+        int[][] nowInfo = cctvInfo[cctv.camType]; //ex) cctvInfo[2] = {{0,1},{2,3}}; // {{상,하}, {좌,우}}
+
+        // {0,1} 색칠하게 하고 기다려... 돌아오면 {2,3} 색칠하게 하고 기다려. 그러려면 일단 지도 사본에다 칠해야지!
+        for(int r=0; r<nowInfo.length; r++){ // 회전 반복문. 1,3,4번 카메라는 4번반복, 2번카메라는 두번 반복함.
+            int[][] copy = deepcopy(prev); // 내가 색칠하기 전 지도를 복사뜸!
+            // {0,1} 또는 {0,2,3} 방향에 색칠해야 됨.
+            int[] now = nowInfo[r]; // nowInfo{0,1} 이런 1차 배열이 나옴. 현재 카메라가 쳐다보는 방향들임.
+            for(int d=0; d<now.length; d++){ // 1번 카메라는 1방향, 5번 카메라는 4방향
+                draw(d, cctv.i, cctv.j, copy);
+            }
+            dfs(camIdx+1, copy); // 내가 방금 상,하 색칠한거 줄게. 나 기다린다?
+        }
+    }
+
+    static void draw(int dir, int si, int sj, int[][] copy){ // 주어진 좌표에서 주어진 방향으로 쭉 직진하면서 색칠하는
+        int nexti = si + di[dir];
+        int nextj = sj + dj[dir];
+        while(true){
+            if(nexti<0 || nexti>=N || nextj<0 || nextj>=M || copy[nexti][nextj]==6) break;
+            copy[nexti][nextj] = 7; // 뛰쳐나가지 않고 벽 안만났으면
+            nexti += di[dir];
+            nextj += dj[dir];
+        }
+    }
+
+    static int[][] deepcopy(int[][] origin){
+        int[][] copy = new int[origin.length][origin[0].length];
+
+        for(int i=0; i<origin.length; i++) {
+            for(int j=0; j<origin[i].length; j++) {
+                copy[i][j] = origin[i][j];
+            }
+        }
+        return copy;
+    }
 }
